043805fe54d14ef3d24735375df1f387c62e7896
Abhijeet Dhupia <abhijeetd@iisc.ac.in>
2021-04-01 15:22:14 +0530

    Delete test.py

--- configs/test.py
+++
@@ -1,8 +0,0 @@
-import yaml 
-with open("config.yaml", "r") as ymlfile:
-    cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
-
-# print(cfg['dataset']['mean_vals'])
-print(cfg['abnormalities']['both'])
-print(cfg['seed'])
-print(cfg['dataset']['train_dir'])
